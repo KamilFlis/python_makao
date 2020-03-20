2ea3e29f93cd2431b40d445d624638f5792a35d3
kamil <kam.flis99@gmail.com>
2020-03-20 04:18:55 +0100

    Added win condition, method to put cards on table

--- venv/bin/makao.py
+++ venv/bin/makao.py
@@ -3,8 +3,11 @@ import random
 from enum import Enum
 from enum import IntEnum
 
-# path to card images
-cards_path = '/home/kamil/Code/Python/Makao/cards_resized_renamed/'
+# # path to card images
+# cards_path = '/home/kamil/Code/Python/Makao/cards_resized_renamed/'
+#
+# # screen resolution
+# screen_width, screen_height = 1024, 768
 
 # card values - name + value
 class CardValue(IntEnum):
@@ -28,24 +31,33 @@ class CardSuit(Enum):
     HEARTS = 'Hearts'
     DIAMONDS = 'Diamonds'
 
+# create special cards set
+specialCards = set()
+specialCardValues = [CardValue.Two, CardValue.Three, CardValue.Four, CardValue.Jack, CardValue.Ace]
+for card_suit in CardSuit:
+     for card_val in specialCardValues:
+         specialCards.add((card_val.value, card_suit.value))
 
-# special cards in game - 2, 3, 4, jack, ace, king of spades and king of hearts
-specialCards = { CardValue.Two, CardValue.Three, CardValue.Four, CardValue.Jack,
-                 CardValue.Ace, (CardValue.King, CardSuit.SPADES), (CardValue.King, CardSuit.HEARTS)}
+specialCards.add((CardValue.King.value, CardSuit.SPADES.value))
+specialCards.add((CardValue.King.value, CardSuit.HEARTS.value))
 
 # card info
 class Card:
-    def __init__(self, suit, value, img_path):
+    def __init__(self, suit, value):
         self.suit = suit
         self.value = value
-        self.image = pygame.image.load(img_path)
 
     def show(self):
         print("{} of {}".format(self.value.name, self.suit.value))
 
     def is_special(self):
-        return self in specialCards
+        return specialCards.__contains__((self.value.value, self.suit.value))
 
+    # check if card can be put on table (same color or value)
+    def is_playable(self, other):
+        if self.suit is other.suit or self.value is other.value:
+            return True
+        return False
 
 # deck info and methods
 class Deck:
@@ -56,7 +68,7 @@ class Deck:
     def build(self):
         for suit in CardSuit:
             for val in CardValue:
-                self.cards.append(Card(suit, val, cards_path + '{}{}.png'.format(val.value, suit.value)))
+                self.cards.append(Card(suit, val))
 
     def shuffle(self):
         random.shuffle(self.cards)
@@ -71,21 +83,13 @@ class Deck:
     def count(self):
         return len(self.cards)
 
-    # special cards - temporary method
-    def wypisz(self):
-        counter=0
-        for card in self.cards:
-            if card.is_special():
-                counter+=1
-                card.show()
-        print(counter)
-
 
 #player info
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, id):
         self.hand = []
         self.name = name
+        self.id = id
 
     def show_hand(self):
         for card in self.hand:
@@ -94,7 +98,7 @@ class Player:
 
 class Game:
     def __init__(self):
-        self.players = [Player("Zygrfryda"), Player("Computer")]
+        self.players = [Player("Zygrfryda", 0), Player("Computer", 1)]
         self.deck = Deck()
         self.table = []
         self.deal()
@@ -103,81 +107,113 @@ class Game:
     def deal(self):
         self.deck.shuffle()
         # add 5 cards to player's hand
-        for card in range(0, 5):
+        for card in range(5):
             for player in self.players:
                 player.hand.append(self.deck.draw())
 
-
-        while len(self.table) is 0:    # <------- condition to loop if first card was special
-            topCard = self.deck.cards[len(self.deck.cards) - 1]
-            # Checks if first card on table isn't special
-            if not topCard.is_special():
+        while not self.table:    # <------- condition to loop if first card was special
+            # Checks if first card on table isn't special else shuffles deck
+            if not self.deck.cards[-1].is_special():
                 self.table.append(self.deck.draw())
             else:
                 self.deck.shuffle()
 
-        print("Cards in deck:", self.deck.count())
-        print("On table: ")
-        self.table[0].show()
-
-
-##########################
     def put_on_table(self):
-        for i in range(0, len(self.players[0].hand)):
-            print(i, ":", end =' ')
-            self.players[0].hand[i].show()     #self.players[0].show_hand()
-        cardToPutOnTable = input("Select: ")
-        cardToPutOnTable = int(cardToPutOnTable)
-        #add buttons on cards
-        self.table.append(self.players[0].hand.pop(cardToPutOnTable))
+        # show top of table
+        print("Table: ", end = '')
+        self.table[-1].show()
 
-        for card in self.table:
-            card.show()
+        # show cards in hand to select
+        for i in range(len(self.players[0].hand)):
+            print(i, ":", end = ' ')
+            self.players[0].hand[i].show()
+        print(len(self.players[0].hand), ": Draw card")
 
-##################
+        how_many_cards_can_you_play = 0
+        # checks if you can put any card on table - if not automatically draws card
+        for card in self.players[0].hand:
+            if card.is_playable(self.table[-1]):
+                how_many_cards_can_you_play += 1
+
+        if how_many_cards_can_you_play is 0:
+            print("You couldn't put any card on table, Drawing card from deck...")
+            self.draw_card(0)
+        else:
+            print("Number of cards you can play:", how_many_cards_can_you_play)
+            card_to_put_on_table = input("Select: ")
+            card_to_put_on_table = int(card_to_put_on_table)
+
+            # option to draw card
+            if card_to_put_on_table is len(self.players[0].hand):
+                self.draw_card(0)
+            # check i card can be put on table
+            elif self.can_put_on_table(card_to_put_on_table):
+                self.table.append(self.players[0].hand.pop(card_to_put_on_table))
 
 
     # checks restrictions made by special cards and checks if card can be put on table
-    def can_put_on_table(self):
-        pass
+    # one method should do one thing? -- for every special card different method? - different restrictions
+    def can_put_on_table(self, card):
+        if not self.players[0].hand[card].is_playable(self.table[-1]):
+            print("Can't put this card on table")
+            return False
+        return True
+
+    # win condition - empty hand means player won
+    def win_con(self, player_id):
+        if not self.players[player_id].hand:
+            print("You won")
+            exit(0)  # change game loop cond?
+
+    # draw one card --> put in player's hand
+    def draw_card(self, player_id):
+        self.players[player_id].hand.append(self.deck.draw())
 
 
-carddd = Card(CardValue.King, CardSuit.HEARTS, "/home/kamil/Code/Python/Makao/cards_resized/KH.png")
-print(str(carddd.suit.value)+str(carddd.value.value))
-
-# main function
 if __name__ == "__main__":
 
-
-    # pygame initialization
-    pygame.init()
-
-    # create screen
-    screen_width, screen_height = 1024, 768
-    screen = pygame.display.set_mode((screen_width, screen_height))
-
-    # title
-    pygame.display.set_caption("Makao")
-
-
     makao = Game()
+    while not makao.win_con(0):
+        makao.put_on_table()
 
 
-
-    running = True
-    while running:
-
-        # RGB color
-        # greenish background
-        screen.fill((3, 122, 48))
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        pygame.display.update()
-
-
-    pygame.quit()
-
+# main function
+# if __name__ == "__main__":
+#
+#     # pygame initialization
+#     pygame.init()
+#
+#     # create screen
+#     screen = pygame.display.set_mode((screen_width, screen_height))
+#
+#     # title
+#     pygame.display.set_caption("Makao")
+#
+#     makao = Game()
+# #    screen.fill((3, 122, 48))
+# #    makao.players[0].show_hand()
+# #    makao.deck.show()
+# #    makao.players[0].show_hand()
+#
+#   #  pygame.display.update()
+#
+#     running = True
+#     while running:
+#         # RGB color
+#         # greenish background
+#    #     screen.fill((3, 122, 48))
+#
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 running = False
+#             if event.type == pygame.MOUSEBUTTONDOWN:
+#                 i = 1
+#                 x -= 15
+#                 makao.draw_card()
+#
+#
+#         makao.players[0].show_hand()
+#         pygame.display.update()
+#
+#     pygame.quit()
 
